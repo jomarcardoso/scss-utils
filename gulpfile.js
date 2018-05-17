08f210324722ca65ee5bb2bca6369926d9ed7287
jomar.cardoso <jomar.cardoso@cwi.com.br>
2018-05-17 20:42:19 -0300

    start

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+
+'use strict'
+
+gulp.task('sass', function () {
+  return gulp.src('./sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+});
+
+gulp.task('sass:watch', function () {
+  gulp.watch('./sass/**/*.scss', ['sass']);
+});
+
+gulp.task('default', ['watch', 'sass:watch']);
